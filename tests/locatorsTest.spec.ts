e12c03cd36161c2c340164370f48bde457d7a483
anand.sitaram <anandbs291@gmail.com>
2024-04-09 11:42:50 +0530

    First Commit

--- tests/locatorsTest.spec.ts
+++ tests/locatorsTest.spec.ts
@@ -25,5 +25,25 @@ test('locators',async ({page})=>{
    // await page.locator('input[name="username"]').fill("admin")
 
     //By xpath - Not recommended -https://playwright.dev/docs/other-locators
-    await page.locator("//input[@id='username']").fill("admin")
+    //await page.locator("//input[@id='username']").fill("admin")
+
+    //By partial text
+   // await  page.locator(":text('Cont')").click()
+
+    //By exact text
+    //await  page.locator(":text-is('Continue')").click()
+
+    /*await page.getByRole('textbox',{name:"username"}).fill("admin")
+
+    await page.getByText("Continue").click()
+
+   await page.getByLabel("password").fill("Admin123")
+
+    await page.getByText("Log in").click()
+    await page.getByPlaceholder("password").click()
+
+     await page.getByTitle("IOT Dashboard").click()
+
+    //Need to pass data-testid attribute value
+    await  page.getByTestId("searchPatientIcon").click()*/
 })
